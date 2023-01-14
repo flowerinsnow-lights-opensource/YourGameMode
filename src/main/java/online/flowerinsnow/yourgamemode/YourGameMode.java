41293a6e0572fa78d48377c7e720f98db4814125
冬花ice <flowerinsnow@donghua.email.cn>
2023-01-14 14:20:28 +0000

    Update version

--- src/main/java/online/flowerinsnow/yourgamemode/YourGameMode.java
+++ src/main/java/online/flowerinsnow/yourgamemode/YourGameMode.java
@@ -16,7 +16,7 @@ import online.flowerinsnow.yourgamemode.listener.NetworkPlayerInfoListener;
 @SideOnly(Side.CLIENT)
 public class YourGameMode {
     public static final String MODID = "yourgamemode";
-    public static final String VERSION = "1.12.1";
+    public static final String VERSION = "1.12.2";
     
     @EventHandler
     public void init(FMLInitializationEvent event) {
