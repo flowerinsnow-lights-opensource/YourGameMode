19d025a687657485ab1f93d7e6ad5731a818a928
冬花ice <flowerinsnow@donghua.email.cn>
2023-02-03 16:12:52 +0800

    Try to fix internationalization

--- src/main/java/online/flowerinsnow/yourgamemode/YourGameMode.java
+++ src/main/java/online/flowerinsnow/yourgamemode/YourGameMode.java
@@ -16,7 +16,7 @@ import online.flowerinsnow.yourgamemode.listener.NetworkPlayerInfoListener;
 @SideOnly(Side.CLIENT)
 public class YourGameMode {
     public static final String MODID = "yourgamemode";
-    public static final String VERSION = "1.12.2";
+    public static final String VERSION = "1.12.3";
     
     @EventHandler
     public void init(FMLInitializationEvent event) {
